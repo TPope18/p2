53246e884c00de1f90c3b1800f29ef161ba7ed17
Areeba Rahu <133078376+rrareeba@users.noreply.github.com>
2023-09-18 13:15:40 -0400

    Update template-processor.js

--- template-processor.js
+++ template-processor.js
@@ -1,10 +1,18 @@
+'use strict';
+
 class TemplateProcessor {
     constructor(template) {
         this.template = template;
     }
-    fillIn(dictionary) {
-        return this.template.replace(/ /g, (match, property) => {
-            return dictionary[property] || '';
-        });
+    fillIn(dictionary){
+        let returnString = this.template;
+        for (const property in dictionary){
+            if (Object.prototype.hasOwnProperty.call(dictionary, property)) {
+                returnString = returnString.replaceAll("{{" + property + "}}",dictionary[property]);
+            }
+        }
+        const regex = /{{.*}}/g;
+        returnString = returnString.replaceAll(regex,"");
+        return returnString;
     }
 }
